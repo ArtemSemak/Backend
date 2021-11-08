847f02254c96705f0a232554daa07d8450c0e187
Artem <artemsemak2000@gmail.com>
2021-11-08 17:13:07 +0300

    change for heroku

--- todoModel.js
+++ todoModel.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize(process.env.DB_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 const ToDo = sequelize.define(
     'ToDo',
