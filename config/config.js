2c054a4d23d5e3ece4322a791004512cf4dd9e84
Artem <artemsemak2000@gmail.com>
2021-11-08 17:21:38 +0300

    edit config

--- config/config.js
+++ config/config.js
@@ -3,14 +3,23 @@ require('dotenv').config()
 module.exports = {
   "development": {
     "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    }
   },
   "test": {
     "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    }
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    }
   }
 }
