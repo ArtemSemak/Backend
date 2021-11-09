94b35b24f87bf727a75655a91f19e6fd331fd21a
Artem <artemsemak2000@gmail.com>
2021-11-09 16:02:52 +0300

    fix bugs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.options('*', (req,res) => { res.sendStatus(200); });
+
 
 
   app.options("/*", function(req, res, next){
