9f1a0b99e2db12f7566ba781707fdfddda4717b7
Artem <artemsemak2000@gmail.com>
2021-11-09 10:58:39 +0300

    add headers

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use("/api", async (req, res, next) => {
   try {
     if (!req.headers["x-auth"]) return res.sendStatus(401);
